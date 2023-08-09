c3384daf5b3d1cd2ea1d0ac6e553e19bb36ca78d
developersajeeb <sajeebdeveloper@gmail.com>
2023-08-09 21:56:27 +0600

    add some folders and add home page

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -2,10 +2,17 @@ import {
     createBrowserRouter,
 } from "react-router-dom";
 import MainLayouts from "../Layouts/MainLayouts";
+import Home from "../pages/Home/Home";
 
 export const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayouts></MainLayouts>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        }
+      ]
     },
   ]);
