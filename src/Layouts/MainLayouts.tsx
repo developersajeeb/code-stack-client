d591af0206f9c0c8b2bd96c21687e2344b2a6e3e
fakhrul-hasan <mdfakhrulh7@gmail.com>
2023-08-10 22:27:57 +0600

    Strategy section added

--- src/Layouts/MainLayouts.tsx
+++ src/Layouts/MainLayouts.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar/NavBar";
-import Starmind from "../components/Starmind/Starmind";
 
 const MainLayouts = () => {
     return (
         <>
             <NavBar></NavBar>
             <Outlet></Outlet>
-            <Starmind></Starmind>
         </>
     );
 };
