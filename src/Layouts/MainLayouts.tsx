6c1c67e8f36ce5132a13004560f85802e5eb44eb
bilkish-akther <bilkishakther921@gmaile.com>
2023-08-17 13:56:00 +0600

    Add Question Section css

--- src/Layouts/MainLayouts.tsx
+++ src/Layouts/MainLayouts.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar/NavBar";
 import Starmind from "../components/Starmind/Starmind";
+import AddQuestion from "../components/Home/AddQuestion/AddQuestion";
 
 const MainLayouts = () => {
     return (
@@ -8,6 +9,8 @@ const MainLayouts = () => {
             <NavBar></NavBar>
             <Outlet></Outlet>
             <Starmind></Starmind>
+            <AddQuestion></AddQuestion>
+        
         </>
     );
 };
