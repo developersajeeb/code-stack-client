4a65ca81a82c4075db5daca1aff1a940434c84e8
developersajeeb <sajeebdeveloper@gmail.com>
2023-08-28 23:31:35 +0600

    fix some problem in navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -33,7 +33,7 @@ const NavBar = () => {
     
 
     return (
-        <nav className='shadow-md px-2 py-3 md:px-8 lg:px-32 border-t-4 border-[#5138EE] flex items-center gap-4 md:gap-10 bg-transparent bg-white'>
+        <nav className='shadow-md px-2 py-3 md:px-8 lg:px-32 border-t-4 border-[#33B89F] flex items-center gap-4 md:gap-10 bg-transparent bg-white'>
             <div className='flex items-center gap-4'>
                 <span onClick={() => { setOpen(!isOpen) }}><Hamburger size={25}></Hamburger></span>
                 <ul className={`drop-shadow-xl grid font-medium text-gray-600 bg-white px-8 py-6 rounded-md absolute w-60 duration-300 border-2 border-dashed ${isOpen ? 'left-4 lg:left-32 top-20' : 'top-20 -left-60'}`}>
@@ -48,7 +48,7 @@ const NavBar = () => {
                             <>
                                 <figure className='w-10 h-10 mb-5'>
                                     <Link to={`/my-profile/${user?.email}`}>
-                                        <img className='rounded-full' src={user?.photoURL || ''} alt="" />
+                                        <img className='rounded-full' src={userData?.imgURL || ''} alt="" />
                                     </Link>
                                 </figure>
                                 <button className='small-btn transparent-button-small w-12' onClick={logOut}><BiLogInCircle /></button>
